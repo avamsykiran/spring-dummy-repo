bcc91545304b2586f7e516ec1f65cc6a4ccdb1e6
Vamsy Kiran A <a.vamc.it@gmail.com>
2023-05-19 09:58:08 +0530

    first error fixed

--- src/main/java/com/cts/demo/controllers/WelcomeController.java
+++ src/main/java/com/cts/demo/controllers/WelcomeController.java
@@ -2,7 +2,7 @@ package com.cts.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping({"","/","/home"})
